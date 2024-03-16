78cf0541ca294723af0049e20389bd5e64906577
BellGakk <297635067@qq.com>
2024-03-16 06:17:32 +0000

    mart_xiangyu

---
+++ mart/train_resnet_loat.sh
@@ -0,0 +1,2 @@
+python train_resnet.py --backbone resnet18 --gpu_id cuda:0 --batch-size 512 --reg --theta 0.1 --reg_type mse --LORE-type LORE_v1
+python train_resnet.py --backbone resnet18 --gpu_id cuda:0 --batch-size 512 --reg --theta 0.1 --reg_type mse --LORE-type LORE
