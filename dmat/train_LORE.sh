e3eeba3b6df975d5d5c0640dcdc324e796f0b547
GeraldXXXTent <32293023+BellGakk@users.noreply.github.com>
2024-07-02 20:03:17 +0100

    Update train_LORE.sh

--- dmat/train_LORE.sh
+++ dmat/train_LORE.sh
@@ -1,7 +1,7 @@
-python train-wa-LORE.py --data-dir 'imagenet-data' \
+python train-wa-LOAT.py --data-dir 'tiny-imagenet-data'\
     --log-dir 'trained_models' \
-    --desc 'preresnet18_tiny-imagenet_lr0p2_Trades5_epoch100_bs512_fraction0p7_ls0p1_LORE_v1' \
-    --data tiny-imagenet \
+    --desc 'prn18_tiny-imagenet_lr0p2_Trades5_epoch100_bs512_fraction0p7_ls0p1_LORE' \
+    --data tiny-imagenets \
     --batch-size 512 \
     --batch-size-validation 128 \
     --model preact-resnet18\
@@ -11,39 +11,56 @@ python train-wa-LORE.py --data-dir 'imagenet-data' \
     --unsup-fraction 0.7 \
     --aux-data-filename '~/DM-Improves-AT/tinyimagenet_aug/tiny_edm_1m.npz' \
     --ls 0.1 \
-    --LORE_type LORE_v1\
-    --gpu_id 2
+    --gpu_id 0 \
+    --LORE_type LORE
 
-python train-wa-LORE.py --data-dir 'imagenet-data' \
+python train-wa-LOAT.py --data-dir 'tiny-imagenet-data'\
     --log-dir 'trained_models' \
-    --desc 'preresnet18_tiny-imagenet_lr0p2_MART_epoch100_bs512_fraction0p7_ls0p1_LORE_v1' \
-    --data tiny-imagenet \
+    --desc 'prn18_tiny-imagenet_lr0p2_Trades5LSE_epoch100_bs512_fraction0p7_ls0p1' \
+    --data tiny-imagenets \
     --batch-size 512 \
     --batch-size-validation 128 \
     --model preact-resnet18\
     --num-adv-epochs 100 \
-    --lr 0.2 \
-    --mart \
-    --beta 5.0 \
-    --unsup-fraction 0.7 \
-    --aux-data-filename '~/DM-Improves-AT/tinyimagenet_aug/tiny_edm_1m.npz' \
-    --ls 0.1 \
-    --LORE_type LORE_v1\
-    --gpu_id 2
-
-python train-wa-LORE.py --data-dir 'imagenet-data' \
-    --log-dir 'trained_models' \
-    --desc 'preresnet18_tiny-imagenet_lr0p2_TRADES5LSE_epoch100_bs512_fraction0p7_ls0p1_LORE_v1' \
-    --data tiny-imagenet \
-    --batch-size 512 \
-    --batch-size-validation 128 \
-    --model preact-resnet18\
-    --num-adv-epochs 100 \
-    --lr 0.2 \
     --LSE \
+    --lr 0.2 \
     --beta 5.0 \
     --unsup-fraction 0.7 \
     --aux-data-filename '~/DM-Improves-AT/tinyimagenet_aug/tiny_edm_1m.npz' \
     --ls 0.1 \
-    --LORE_type LORE_v1\
-    --gpu_id 2
+    --gpu_id 0 \
+    --LORE_type None
+
+python train-wa-LOAT.py --data-dir 'tiny-imagenet-data'\
+    --log-dir 'trained_models' \
+    --desc 'prn18_tiny-imagenet_lr0p2_Trades5LSE_epoch100_bs512_fraction0p7_ls0p1_LORE_v1' \
+    --data tiny-imagenets \
+    --batch-size 512 \
+    --batch-size-validation 128 \
+    --model preact-resnet18\
+    --num-adv-epochs 100 \
+    --LSE \
+    --lr 0.2 \
+    --beta 5.0 \
+    --unsup-fraction 0.7 \
+    --aux-data-filename '~/DM-Improves-AT/tinyimagenet_aug/tiny_edm_1m.npz' \
+    --ls 0.1 \
+    --gpu_id 0 \
+    --LORE_type LORE_v1
+
+python train-wa-LOAT.py --data-dir 'tiny-imagenet-data'\
+    --log-dir 'trained_models' \
+    --desc 'prn18_tiny-imagenet_lr0p2_Trades5LSE_epoch100_bs512_fraction0p7_ls0p_LORE' \
+    --data tiny-imagenets \
+    --batch-size 512 \
+    --batch-size-validation 128 \
+    --model preact-resnet18\
+    --num-adv-epochs 100 \
+    --LSE \
+    --lr 0.2 \
+    --beta 5.0 \
+    --unsup-fraction 0.7 \
+    --aux-data-filename '~/DM-Improves-AT/tinyimagenet_aug/tiny_edm_1m.npz' \
+    --ls 0.1 \
+    --gpu_id 0 \
+    --LORE_type LORE
